4e2c03bba14a07412a8a19a3b930961043dcf74f
DavidFraga09 <2302079@utrivieramaya.edu.mx>
2024-10-03 19:06:42 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const navItems = document.querySelectorAll('.nav-item');
-    navItems.forEach((item, index) => {
-        setTimeout (()=>{
-            item.classList.add('show')
-        
-        }, 100 * index);
-    })
-})
